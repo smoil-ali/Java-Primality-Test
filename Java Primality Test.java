d152ff61c92f1e93d00871893680e2efc1e53a03
smoil-ali <40652937+smoil-ali@users.noreply.github.com>
2019-06-11 22:34:34 -0700

    Create Java Primality Test.java

---
+++ Java Primality Test.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+        scanner.close();
+        BigInteger bi1;
+        bi1 = new BigInteger(n);
+        System.out.println((bi1.isProbablePrime(1))?"prime":"not prime");
+        
+    }
+}
